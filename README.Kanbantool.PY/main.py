d7298a54d2995ed7d7e661cbf656974b61e983f1
Luã Govinda <govinda777@gmail.com>
2022-07-10 15:54:38 -0300

    implementacao de menu dinamico

---
+++ README.Kanbantool.PY/main.py
@@ -0,0 +1,20 @@
+from kanbantool import *
+
+def main():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("(1) Forca (2) Adivinhação")
+
+    jogo = int(input("Qual jogo? "))
+
+    if(jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+
+if(__name__ == "__main__"):
+    main()
